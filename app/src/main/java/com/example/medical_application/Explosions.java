b723c49fc43f94f618a44ae1cadf1c8caa8e4ec8
bahaahafiz <bahaahafiz1@gmail.com>
2021-05-21 23:23:11 +0200

    اضافة صفحة لاسعافات الانفجارات

---
+++ app/src/main/java/com/example/medical_application/Explosions.java
@@ -0,0 +1,14 @@
+package com.example.medical_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Explosions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explosions);
+    }
+}
